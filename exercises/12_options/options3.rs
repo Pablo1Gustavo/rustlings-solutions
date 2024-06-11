847d0c69b58c398e05036a23c403c1b25ae11e3c
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2024-06-11 01:05:14 -0300

    Finish options exercises

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -13,9 +13,10 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => panic!("no match!"),
+    match y
+    {
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        _           => panic!("no match!"),
     }
     y; // Fix without deleting this line.
 }
