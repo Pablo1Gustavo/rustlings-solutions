788da9cfb2e4492258cfd4a796566925f41b289a
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2024-06-11 01:05:07 -0300

    Finish hashmaps exercises

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -14,12 +14,11 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
-enum Fruit {
+enum Fruit
+{
     Apple,
     Banana,
     Mango,
@@ -27,7 +26,8 @@ enum Fruit {
     Pineapple,
 }
 
-fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
+fn fruit_basket(basket: &mut HashMap<Fruit, u32>)
+{
     let fruit_kinds = vec![
         Fruit::Apple,
         Fruit::Banana,
@@ -36,10 +36,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Pineapple,
     ];
 
-    for fruit in fruit_kinds {
+    for fruit in fruit_kinds
+    {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if !basket.contains_key(&fruit)
+        {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -15,21 +15,22 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
-struct Team {
-    goals_scored: u8,
+struct Team
+{
+    goals_scored  : u8,
     goals_conceded: u8,
 }
 
-fn build_scores_table(results: String) -> HashMap<String, Team> {
+fn build_scores_table(results: String) -> HashMap<String, Team>
+{
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
-    for r in results.lines() {
+    for r in results.lines()
+    {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
         let team_1_score: u8 = v[2].parse().unwrap();
@@ -40,6 +41,27 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores
+            .entry(team_1_name)
+            .and_modify(|team| {
+                team.goals_scored   += team_1_score;
+                team.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                goals_scored  : team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        scores
+            .entry(team_2_name)
+            .and_modify(|team| {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
     scores
 }
